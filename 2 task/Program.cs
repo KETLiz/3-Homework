3cd1ac5c49bea476e788ccb57e257e77ad84991b
KET <varlamova_e.t@mail.ru>
2022-08-29 10:09:28 +0300

    ghjk

---
+++ 2 task/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число n для составления таблицы кубов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+{
+    Console.WriteLine(Math.Pow(i++, 3));
+}
