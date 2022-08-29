3856ad2bb074eec99a4dd540e4510fec6929396b
KET <varlamova_e.t@mail.ru>
2022-08-29 10:42:04 +0300

    123

---
+++ 1 task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine);
